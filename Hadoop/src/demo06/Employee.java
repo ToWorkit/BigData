693440cd4f09de4107a95afaa00df1a4218cdeaf
Doing <1211126374@qq.com>
2018-04-17 18:30:41 +0800

    20180417

---
+++ Hadoop/src/demo06/Employee.java
@@ -0,0 +1,23 @@
+package demo06;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+public class Employee implements Writable {
+
+	@Override
+	public void readFields(DataInput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void write(DataOutput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

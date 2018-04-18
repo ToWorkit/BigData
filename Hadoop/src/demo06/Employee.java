0504386c44b5fc1fa1e0c8bdf02f1e66a9393050
Doing <1211126374@qq.com>
2018-04-18 10:14:44 +0800

    20180418

--- Hadoop/src/demo06/Employee.java
+++ Hadoop/src/demo06/Employee.java
@@ -6,18 +6,104 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.Writable;
 
+//数据: 7654,MARTIN,SALESMAN,7698,1981/9/28,1250,1400,30
 public class Employee implements Writable {
-
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private String hiredate;
+	private int sal;
+	private int comm;
+	private int deptno;
+	
 	@Override
-	public void readFields(DataInput arg0) throws IOException {
-		// TODO Auto-generated method stub
-		
+	public void readFields(DataInput input) throws IOException {
+		// 读数据 -> 反序列化
+		this.empno = input.readInt();
+		this.ename = input.readUTF();
+		this.job = input.readUTF();
+		this.mgr = input.readInt();
+		this.hiredate = input.readUTF();
+		this.sal = input.readInt();
+		this.comm = input.readInt();
+		this.deptno = input.readInt();
 	}
 
 	@Override
-	public void write(DataOutput arg0) throws IOException {
-		// TODO Auto-generated method stub
-		
+	public void write(DataOutput output) throws IOException {
+		// 给(写)数据 -> 序列化
+		output.writeInt(this.empno);
+		output.writeUTF(this.ename);
+		output.writeUTF(this.job);
+		output.writeInt(this.mgr);
+		output.writeUTF(this.hiredate);
+		output.writeInt(this.sal);
+		output.writeInt(this.comm);
+		output.writeInt(this.deptno);
 	}
 
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getMgr() {
+		return mgr;
+	}
+
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+
+	public String getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+	public int getComm() {
+		return comm;
+	}
+
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
 }

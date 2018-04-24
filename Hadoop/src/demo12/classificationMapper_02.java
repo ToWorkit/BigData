d83b23d7752e4b10bdb7910079ee2d0fc2a090ca
Doing <1211126374@qq.com>
2018-04-24 11:39:48 +0800

    20180424

---
+++ Hadoop/src/demo12/classificationMapper_02.java
@@ -0,0 +1,29 @@
+package demo12;
+
+/*		A	a-e-d-c
+		B	b-a
+		C	b-e-a
+		D	c-b-a
+		E	d-c
+		F	d-e
+		*/
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+// k4 v4 k5 v5
+public class classificationMapper_02 extends Mapper<Text, Text, Text, Text>{
+	@Override
+	protected void map(Text k4, Text v4, Mapper<Text, Text, Text, Text>.Context context)
+			throws IOException, InterruptedException {
+		// 数据
+		String data = v4.toString();
+		// 分词处理
+		String upper = data.split("\t")[0];
+		String[] lower = data.split("\t")[1].split("-");
+		for (String str: lower) {
+			context.write(new Text(str), new Text(upper));
+		}
+	}
+}
---
+++ Hadoop/src/demo12/classificationReducer_02.java
@@ -0,0 +1,17 @@
+package demo12;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+// k6 v6 k7 v7
+public class classificationReducer_02 extends Reducer<Text, Text, Text, Text>{
+	@Override
+	protected void reduce(Text k6, Iterable<Text> v6, Reducer<Text, Text, Text, Text>.Context context)
+			throws IOException, InterruptedException {
+		String name = "";
+		for (Text v: v6) {
+			context.write(new Text(k6), new Text(v));
+		}
+	}
+}

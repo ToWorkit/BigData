59c99e8dc4cde98073f97a952db16096ab8dd813
Toworkit <1211126374@qq.com>
2018-05-13 15:09:40 +0800

    20180513

---
+++ Scala_02/src/main/scala/Scala_Server/WordCount.scala
@@ -0,0 +1,9 @@
+package Scala_Server
+import org.apache
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    // 创建一个Config
+    val conf = new SparkConf().setAppName("MyScalaWordCount")
+  }
+}

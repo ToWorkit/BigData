581d4c1a2ad7850267542cbcbced8c96f9b873a4
Toworkit <1211126374@qq.com>
2018-05-18 07:54:41 +0800

    20180518

--- Spark_Server/src/main/scala/SpecifyingSchema.scala
+++ Spark_Server/src/main/scala/SpecifyingSchema.scala
@@ -1,8 +1,9 @@
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
-class SpecifyingSchema {
+object SpecifyingSchema {
   def main(args: Array[String]): Unit = {
     // 创建一个Spark Session对象
     val spark = SparkSession.builder().master("local").appName("SpecifyingSchema").getOrCreate()
@@ -10,6 +11,27 @@ class SpecifyingSchema {
     val data = spark.sparkContext.textFile("D:\\test.txt").map(_.split(" "))
 
     // 创建Schema的结构
-    val schema = StructType
+    val schema = StructType(
+      List(
+        StructField("id", IntegerType, true),
+        StructField("name", StringType, true),
+        StructField("age", IntegerType, true)
+      )
+    )
+
+    // 将数据RDD映射成Row
+    val rowRdd = data.map(p => Row(p(0).toInt, p(1).trim, p(2).toInt))
+
+    // 关联schema
+    val studentDF = spark.createDataFrame(rowRdd, schema);
+
+    // 生成表
+    studentDF.createOrReplaceTempView("student");
+
+    // 执行SQL
+    val result = spark.sql("select * from student")
+
+    // 显示
+    result.show()
   }
 }

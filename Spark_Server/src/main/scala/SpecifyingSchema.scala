319b834b6626113fe9d78979fe185d71cc63acec
Toworkit <1211126374@qq.com>
2018-05-18 08:27:50 +0800

    20180518

--- Spark_Server/src/main/scala/SpecifyingSchema.scala
+++ Spark_Server/src/main/scala/SpecifyingSchema.scala
@@ -1,4 +1,6 @@
 
+import java.util.Properties
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
@@ -23,15 +25,30 @@ object SpecifyingSchema {
     val rowRdd = data.map(p => Row(p(0).toInt, p(1).trim, p(2).toInt))
 
     // 关联schema
-    val studentDF = spark.createDataFrame(rowRdd, schema);
+    val studentDF = spark.createDataFrame(rowRdd, schema)
 
     // 生成表
-    studentDF.createOrReplaceTempView("student");
+    studentDF.createOrReplaceTempView("student")
 
     // 执行SQL
     val result = spark.sql("select * from student")
 
-    // 显示
-    result.show()
+    // 本地显示
+//    result.show()
+
+    // 保存到Oracle中
+    val props = new Properties()
+    // 用户名和密码
+    props.setProperty("user", "scott")
+    props.setProperty("password", "tiger")
+
+    // 会自动创建表，不需要手动创建
+    result.write.jdbc("jdbc:oracle:thin:@192.168.106.11:1521/oracle.example.com", "scott.student", props)
+
+    // 如果表表已经存在了就采用append模式
+    // result.write.mode("append").jdbc("jdbc:oracle:thin:@192.168.106.11:1521/oracle.example.com", "scott.student", props)
+
+    // 完成后停止
+    spark.stop()
   }
 }

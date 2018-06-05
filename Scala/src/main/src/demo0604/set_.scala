3a9225f205c1a6e56690fdcae1aa6361af364fa6
Doing <1211126374@qq.com>
2018-06-05 15:59:23 +0800

    Scala

--- Scala/src/main/src/demo0604/set_.scala
+++ Scala/src/main/src/demo0604/set_.scala
@@ -1,5 +1,5 @@
 package demo0604
-
+import scala.collection.mutable
 /**
   * Set 集，不重复元素的集合，默认实现是 HashSet
   */
@@ -10,5 +10,17 @@ object set_ {
     // 添加重复元素
     s1 += 0
     println(s1) // Set(0, 1, 2, 32, 4)
+
+    // 链表哈希集合
+    var weeksDay = mutable.LinkedHashSet("周一", "周二")
+    // 添加
+    weeksDay += "周五"
+    println(weeksDay) // Set(周一, 周二, 周五)
+    // 判断是否存在
+    println(weeksDay.contains("周三")) // false
+
+    // 可排序的set
+    var s2 = mutable.SortedSet(2, 3, 1, 0, 9, 29, 4)
+    println(s2) // TreeSet(0, 1, 2, 3, 4, 9, 29)
   }
 }

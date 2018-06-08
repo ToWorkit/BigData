6112b7a2c1faa95af7d794b7116dc3ff3db7e4f4
Doing <1211126374@qq.com>
2018-06-08 11:29:50 +0800

    协变出现bug

--- Scala/src/main/src/demo0606/demo_01.scala
+++ Scala/src/main/src/demo0606/demo_01.scala
@@ -2,6 +2,7 @@ package demo0606
 
 /**
   * 协变：一个泛型类接收的泛型参数的参数的值可以是本身的类型或者是子类的类型
+  * + T
   */
 class demo_01 {}
 
@@ -18,11 +19,11 @@ class EatSomething[+ T](t: T) {}
 object demo_01 {
   def main(args: Array[String]): Unit = {
     // 创建一个鸟吃东西的对象
-    var c1: EatSomething[Bird] = new EatSomething[Bird](new Bird)
+    var c1: EatSomething[Sparrow] = new EatSomething[Sparrow](new Sparrow)
 
     // 创建一个动物吃东西的对象
     // EatSomething 必须是协变否则会报错
     // 协变后就可以完成将一个子类Bird付给父类Animal
-    var c2: EatSomething[Animal] = c1
+    var c2: EatSomething[Bird] = c1
   }
 }
